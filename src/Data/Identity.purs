9e05d0f43568823ddb2f41b26f232b9d1afb5a67
Nathan Faubion <nathan@n-son.com>
2017-04-24 12:43:58 -0500

    Add Eq1, Ord1

--- src/Data/Identity.purs
+++ src/Data/Identity.purs
@@ -7,10 +7,12 @@ import Control.Comonad (class Comonad)
 import Control.Extend (class Extend)
 import Control.Lazy (class Lazy)
 
+import Data.Eq (class Eq1)
 import Data.Foldable (class Foldable)
 import Data.Functor.Invariant (class Invariant, imapF)
 import Data.Monoid (class Monoid)
 import Data.Newtype (class Newtype)
+import Data.Ord (class Ord1)
 import Data.Traversable (class Traversable)
 
 newtype Identity a = Identity a
@@ -45,6 +47,12 @@ derive newtype instance lazyIdentity :: Lazy a => Lazy (Identity a)
 
 instance showIdentity :: Show a => Show (Identity a) where
   show (Identity x) = "(Identity " <> show x <> ")"
+
+instance eq1Identity :: Eq1 Identity where
+  eq1 = eq
+
+instance ord1Identity :: Ord1 Identity where
+  compare1 = compare
 
 instance functorIdentity :: Functor Identity where
   map f (Identity x) = Identity (f x)
